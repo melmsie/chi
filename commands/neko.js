4981b3027a2a9ac0cd56b24e2a311925a36d4dc3
Austin Melms <memyselfandlie@gmail.com>
2017-07-22 22:00:28 -0400

    Rewrote neko
    Added a "lewd" version and made it async

--- commands/neko.js
+++ commands/neko.js
@@ -1,15 +1,38 @@
 const Discord = require('discord.js')
 const snekfetch = require('snekfetch')
-exports.run = function (client, msg, args) {
-     if (msg.channel.nsfw) {
-        snekfetch.get('https://nekos.life/api/neko')
-            .then(r => message.channel.send({
-                const embed = new Discord.RichEmbed()
-                .setImage(r.body.neko)
-                .setAuthor(msg.author.tag, msg.author.displayAvatarURL)
-                .setColor(`BLACK`)
-            }).catch(err => msg.channel.send('Error: ```' + err + '```');
-        } else {
-           msg.channel.send('Please be in a nsfw marked channel!')
-      } 
+exports.run = async function (client, msg, args) {
+
+   
+    if (args[0] === 'lewd') {
+        
+        if (!msg.channel.nsfw) return msg.channel.send('I don\'t think I\'m allowed to post those here... Maybe try a NSFW marked channel?')
+        
+        let lewd = await snekfetch.get('https://nekos.life/api/lewd/neko')
+        await msg.channel.send({
+            embed: {
+                color: '5881576',
+                image: {
+                    url: lewd.body.neko
+                },
+                footer: {
+                    text: `Requested by ${msg.author.tag}`
+                }
+            }
+        })
+    } else {
+        let lewd = await snekfetch.get('https://nekos.life/api/neko')
+        await msg.channel.send({
+            embed: {
+                color: '5881576',
+                image: {
+                    url: lewd.body.neko
+                },
+                footer: {
+                    text: `Requested by ${msg.author.tag}`
+                }
+            }
+        })
     }
+
+
+}

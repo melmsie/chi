aa6f981409b1efca34b29341444736a93128d3ac
Austin Melms <memyselfandlie@gmail.com>
2017-07-28 00:01:17 -0400

    renamed dog => puppy, fixed as well

---
+++ commands/puppy.js
@@ -0,0 +1,24 @@
+const snekfetch = require('snekfetch')
+exports.run = async function (client, msg) {
+    getDogPic(msg)
+}
+
+async function getDogPic(msg) {
+    const data = await snekfetch.get('https://random.dog/woof.json')
+    if (data.body.url.includes('.mp4')) {
+        return getDogPic(msg)
+    }
+
+    await msg.channel.send({
+        embed: {
+            title: '🐶',
+            color: '5881576',
+            image: {
+                url: data.body.url
+            },
+            footer: {
+                text: `Requested by ${msg.author.tag}`
+            }
+        }
+    })
+}

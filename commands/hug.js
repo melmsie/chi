f2480fc2b41c89c03958e6d255af36ebe65f28f5
Austin Melms <memyselfandlie@gmail.com>
2017-07-31 03:29:00 -0400

    <3

--- commands/hug.js
+++ commands/hug.js
@@ -1,21 +1,19 @@
-/**
+/*
  * Created by Tom on 7/30/2017.
  */
-const snekfetch = require('snekfetch');
-const config = require('../config.json');
+const snekfetch = require('snekfetch')
+const config = require('../config.json')
 exports.run = async function (client, msg,args) {
-    if (args < 1) return message.reply(" :x: O.o you wanna hug yourself??");
+    if (args < 1) return message.reply(" :x: O.o you wanna hug yourself??")
     snekfetch.get('https://nekos.life/api/hug')
         .set('Key', config.nekokey)
-        .then(r => msg.channel.send(`${args} You got a hug from ${msg.author.username} :heart:`,{
+        .then(r => msg.channel.send(`${args} You got a hug from ${msg.author.username} 💜`,{
         embed: {
             color: 5881576,
             image: {
                 url: r.body.url
             }
         }
-    }).catch(e => console.warn('wew tf happened here ' + e)));
-
-
-};
+    }).catch(e => console.warn('wew tf happened here ' + e)))
+}
 

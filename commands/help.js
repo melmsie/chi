16e5584f00ca5c8fdbe85b72d44e5a99dd059319
Austin Melms <memyselfandlie@gmail.com>
2017-08-01 00:08:24 -0400

    Updated help command with all the new commands

--- commands/help.js
+++ commands/help.js
@@ -12,10 +12,14 @@ exports.run = async function (client, msg) {
 				})
 			},
 			fields: [
+				{ name: '+data', value: 'Learn all about me!' },
+				{ name: '+hug', value: 'Let me hug someone for you!' },
 				{ name: '+invite', value: 'Get my current invite so I can join you!' },
+				{ name: '+kiss', value: 'Every kiss begins with Chi!' },
 				{ name: '+kitty', value: 'Awww, a kitty!' },
 				{ name: '+neko', value: 'Cat girls anyone?' },
 				{ name: '+neko lewd', value: 'LEWD Cat girls anyone?' },
+				{ name: '+pat', value: 'Awh, poor them. Maybe you should pat them?' },
 				{ name: '+puppy', value: 'I love puppies!' },
 			],
 			footer: {

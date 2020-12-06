b6ac4dffe1eb5abf92c5c2bf2db5c02fa81fc1a1
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2020-12-06 16:30:57 -0500

    Updated the invite link
    Since the bot was nothing (i made i bot that i'm physically making so that's why it has a ID now)

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 exports.run = function (client, msg) {
-	msg.channel.send(`Y-You can invite me here using this link, ${msg.author.username}-senpai.\n\n<https://goo.gl/rU5pe5>`)
+	msg.channel.send(`Y-You can invite me here using this link, ${msg.author.username}.\n\n<https://discord.com/oauth2/authorize?client_id=759829820975611955&scope=bot&permissions=519238>`)
 }

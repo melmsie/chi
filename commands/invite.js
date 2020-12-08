13d312a8f0e19228e7c3692244e95f943271ce33
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2020-12-07 20:08:39 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 exports.run = function (client, msg) {
-	msg.channel.send(`Y-You can invite me here using this link, ${msg.author.username}.\n\n<https://discord.com/oauth2/authorize?client_id=759829820975611955&scope=bot&permissions=519238>`)
+	msg.channel.send(`Y-You can invite me here using this link, ${msg.author.username}.\n\n<https://discord.com/oauth2/authorize?client_id=785673868567248916&scope=bot&permissions=519238>`)
 }
